7a67a1763e56f983e80eac63a5942bfb9481724e
vmarak <116581479+vmarak@users.noreply.github.com>
2022-10-24 11:05:11 -0500

    Add files via upload
    Issue with focus in entry control in Android OS 8.1 using Xamarin.Forms 5.0.0.2515

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            entry2.Focus();
+        }
+
+        private void entry2_Completed(object sender, EventArgs e)
+        {
+            entry3.Focus();
+        }
+   
+    }
+}
